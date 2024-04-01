2fb1b70eb426d460dae483af2d92ce4b0629b1c9
Somin Yun <somin455@naver.com>
2024-04-01 20:13:46 +0900

    feat: first init

---
+++ src/main/java/com/kukathonF/backend/KukathonF_Application.java
@@ -0,0 +1,13 @@
+package com.kukathonF.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KukathonF_Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KukathonF_Application.class, args);
+	}
+
+}
