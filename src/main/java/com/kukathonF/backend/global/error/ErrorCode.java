58f738b5db3ed1201d56193b68a1051bcccbbbe6
whereami2048 <whereami2048@gmail.com>
2024-04-06 20:43:36 +0900

    chore: 예외 관련 프로젝트 세팅

---
+++ src/main/java/com/kukathonF/backend/global/error/ErrorCode.java
@@ -0,0 +1,57 @@
+package com.kukathonF.backend.global.error;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ErrorCode {
+    /**
+     * 400 Bad Request
+     */
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    INVALID_MODEL_TYPE(HttpStatus.BAD_REQUEST, "유효하지 않은 드론모델 타입입니다."),
+
+    /**
+     * 401 Unauthorized
+     */
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "리소스 접근 권한이 없습니다."),
+
+
+
+    /**
+     * 403 Forbidden
+     */
+    FORBIDDEN(HttpStatus.FORBIDDEN, "리소스 접근 권한이 없습니다."),
+
+    /**
+     * 404 Not Found
+     */
+    ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "엔티티를 찾을 수 없습니다."),
+    DRONE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 드론입니다."),
+    DRONE_GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 드론그룹입니다."),
+    TEST_RESULT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 검사 결과입니다."),
+
+    /**
+     * 405 Method Not Allowed
+     */
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "잘못된 HTTP method 요청입니다."),
+
+    /**
+     * 409 Conflict
+     */
+    CONFLICT(HttpStatus.CONFLICT, "이미 존재하는 리소스입니다."),
+
+
+
+    /**
+     * 500 Internal Server Error
+     */
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다."),
+    JSON_PARSING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Json 으로 변환할 수 없는 String 입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
