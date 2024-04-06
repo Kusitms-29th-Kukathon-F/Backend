8260467bd07b052f2266311dc999f381ed7bb7fb
whereami0404 <whereami2048@gmail.com>
2024-04-06 23:48:32 +0900

    feat: #2 도파민 점수 엔티티 구현

---
+++ src/main/java/com/kukathonF/backend/domain/entity/DopaminScore.java
@@ -0,0 +1,31 @@
+package com.kukathonF.backend.domain.entity;
+
+import static jakarta.persistence.FetchType.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DopaminScore {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long score;
+    private LocalDateTime date;
+
+    @ManyToOne(fetch = LAZY)
+    User user;
+}
